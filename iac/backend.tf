f93ae4cc179eca3d110f6e4b7e02e96537a7fc0e
logunlaja26 <logunlaja26@gmail.com>
2025-05-14 19:00:20 -0500

    Updated code

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,9 +1,9 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = 
-    key            = 
-    region         = 
-    dynamodb_table = 
+    bucket         = "lyomann-terraform-remote-state"
+    key            = "rentzone-app/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terrraform-state-lock"
   }
 }
